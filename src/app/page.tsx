493b1f5b3e7517ce12a77845f52fa148d68442e9
nishitaniyuki <yuki.nishitani85@gmail.com>
2023-10-23 10:36:54 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,6 @@
-'use client';
-
-import { useState } from 'react';
-
 import { loadMore } from './actions';
+import _Home from './_page';
 
 export default function Home() {
-  const [comments, setComments] = useState<React.JSX.Element[]>([]);
-
-  const handleClick = async function() {
-    const newComment = await loadMore();
-    setComments(prev => [...prev, newComment]);
-  }
-
-  return (
-    <>
-      <button onClick={handleClick}>load more</button>
-      <h2>comments</h2>
-      <ul>
-        {comments.map((comment, i) => {
-          return (
-            <li key={i + 1}>
-              {comment}
-            </li>
-          )
-        })}
-      </ul>
-    </>
-  )
+  return <_Home loadMore={loadMore} />
 }
